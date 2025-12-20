01eeba31f4146dceade9d46689933fd4402b75d9
Roman Vassilchenko <36629811+RomanVassilchenko@users.noreply.github.com>
2025-12-21 01:08:11 +0500

    feat: allow VPN control without sudo password

--- modules/core/system/security.nix
+++ modules/core/system/security.nix
@@ -32,6 +32,31 @@
               command = "/run/current-system/sw/bin/pkill";
               options = [ "NOPASSWD" ];
             }
+            # VPN service control without password
+            {
+              command = "/run/current-system/sw/bin/systemctl start openfortivpn-dahua";
+              options = [ "NOPASSWD" ];
+            }
+            {
+              command = "/run/current-system/sw/bin/systemctl stop openfortivpn-dahua";
+              options = [ "NOPASSWD" ];
+            }
+            {
+              command = "/run/current-system/sw/bin/systemctl restart openfortivpn-dahua";
+              options = [ "NOPASSWD" ];
+            }
+            {
+              command = "/run/current-system/sw/bin/systemctl start openconnect-berekebank";
+              options = [ "NOPASSWD" ];
+            }
+            {
+              command = "/run/current-system/sw/bin/systemctl stop openconnect-berekebank";
+              options = [ "NOPASSWD" ];
+            }
+            {
+              command = "/run/current-system/sw/bin/systemctl restart openconnect-berekebank";
+              options = [ "NOPASSWD" ];
+            }
           ];
         }
       ];
