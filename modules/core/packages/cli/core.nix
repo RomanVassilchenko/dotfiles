7255adef5497a8799f7216718b3bb9e811384f9b
Roman Vassilchenko <36629811+RomanVassilchenko@users.noreply.github.com>
2026-04-13 09:59:37 +0500

    feat(packages): add bitwarden-cli, rbw, and jujutsu to cli packages

--- modules/core/packages/cli/core.nix
+++ modules/core/packages/cli/core.nix
@@ -2,11 +2,14 @@
 {
   environment.systemPackages =
     (with pkgs-stable; [
+      bitwarden-cli # Bitwarden CLI.
+      rbw # Caching Bitwarden CLI.
       delta
       fd
       git
       git-absorb
       git-branchless
+      jujutsu # Git, the full tooling.
       jq
       just
       ripgrep
