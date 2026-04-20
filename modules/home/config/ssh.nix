2a8463183123cf07afa7a735121e820ebff39b59
Roman Vassilchenko <36629811+RomanVassilchenko@users.noreply.github.com>
2026-04-20 19:05:00 +0500

    fix(ssh): materialize home ssh config

--- modules/home/config/ssh.nix
+++ modules/home/config/ssh.nix
@@ -1,51 +1,47 @@
-_: {
-  programs.ssh = {
-    enable = true;
-    enableDefaultConfig = false;
+{ lib, pkgs, ... }:
+let
+  sshConfig = pkgs.writeText "ssh-config" ''
+    Include ~/.ssh/config.d/secrets
 
-    includes = [ "~/.ssh/config.d/secrets" ];
+    Host github.com
+      IdentityFile ~/.ssh/id_personal
+      IdentitiesOnly yes
 
-    matchBlocks = {
-      "github.com" = {
-        identityFile = "~/.ssh/id_personal";
-        identitiesOnly = true;
-      };
+    # Forgejo git server (SSH on port 2222 via Tailscale)
+    # NixOS Forgejo uses 'forgejo' as SSH user, not 'git'
+    Host git.romanv.dev
+      HostName 100.64.0.1
+      Port 2222
+      User forgejo
+      IdentityFile ~/.ssh/id_personal
+      IdentitiesOnly yes
 
-      # Forgejo git server (SSH on port 2222 via Tailscale)
-      # NixOS Forgejo uses 'forgejo' as SSH user, not 'git'
-      "git.romanv.dev" = {
-        hostname = "100.64.0.1";
-        port = 2222;
-        user = "forgejo";
-        identityFile = "~/.ssh/id_personal";
-        identitiesOnly = true;
-      };
+    # Ninkear via Tailscale P2P (direct connection, no tunnel)
+    Host ninkear
+      HostName 100.64.0.1
+      User romanv
+      IdentityFile ~/.ssh/id_personal
 
-      # Ninkear via Tailscale P2P (direct connection, no tunnel)
-      "ninkear" = {
-        hostname = "100.64.0.1";
-        user = "romanv";
-        identityFile = "~/.ssh/id_personal";
-      };
-
-      "*" = {
-        identityFile = "~/.ssh/id_personal";
-        identitiesOnly = true;
-        forwardAgent = false;
-        compression = false;
-        serverAliveInterval = 0;
-        serverAliveCountMax = 3;
-        hashKnownHosts = false;
-        userKnownHostsFile = "~/.ssh/known_hosts";
-        controlMaster = "no";
-        controlPath = "~/.ssh/master-%r@%n:%p";
-        controlPersist = "no";
-        addKeysToAgent = "no";
-
-        extraOptions = {
-          KexAlgorithms = "mlkem768x25519-sha256,sntrup761x25519-sha512@openssh.com,curve25519-sha256,curve25519-sha256@libssh.org,diffie-hellman-group-exchange-sha256";
-        };
-      };
-    };
-  };
+    Host *
+      IdentityFile ~/.ssh/id_personal
+      IdentitiesOnly yes
+      ForwardAgent no
+      Compression no
+      ServerAliveInterval 0
+      ServerAliveCountMax 3
+      HashKnownHosts no
+      UserKnownHostsFile ~/.ssh/known_hosts
+      ControlMaster no
+      ControlPath ~/.ssh/master-%r@%n:%p
+      ControlPersist no
+      AddKeysToAgent no
+      KexAlgorithms mlkem768x25519-sha256,sntrup761x25519-sha512@openssh.com,curve25519-sha256,curve25519-sha256@libssh.org,diffie-hellman-group-exchange-sha256
+  '';
+in
+{
+  # Some Git/IDE integrations reject ~/.ssh/config when Home Manager manages it as a symlink into /nix/store.
+  home.activation.installSshConfig = lib.hm.dag.entryAfter [ "writeBoundary" ] ''
+    ${pkgs.coreutils}/bin/install -d -m 700 "$HOME/.ssh"
+    ${pkgs.coreutils}/bin/install -m 600 ${sshConfig} "$HOME/.ssh/config"
+  '';
 }
