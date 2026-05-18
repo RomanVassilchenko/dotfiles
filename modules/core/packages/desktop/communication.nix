29d4b3fb642eb151cc6422d20cbcfa928da3594d
Roman Vassilchenko <roman.vassilchenko.work@gmail.com>
2026-05-18 17:59:22 +0500

    feat(thunderbird): manage catppuccin theme via policy

--- modules/core/packages/desktop/communication.nix
+++ modules/core/packages/desktop/communication.nix
@@ -4,11 +4,28 @@
   pkgs,
   ...
 }:
+let
+  catppuccinThunderbirdMochaMauve = pkgs.fetchurl {
+    url = "https://raw.githubusercontent.com/catppuccin/thunderbird/main/themes/mocha/mocha-mauve.xpi";
+    sha256 = "0h4zwi76189gwibajxzinzigdvjag04cmsqpqzddc8f1xh82p9q2";
+  };
+in
 {
   environment.systemPackages =
     with pkgs;
     lib.optionals config.dotfiles.features.apps.discord.enable [ vesktop ]
     ++ lib.optionals config.dotfiles.features.apps.telegram.enable [ telegram-desktop ]
-    ++ lib.optionals config.dotfiles.features.apps.thunderbird.enable [ thunderbird ]
     ++ lib.optionals config.dotfiles.features.apps.zapzap.enable [ zapzap ];
+
+  programs.thunderbird = lib.mkIf config.dotfiles.features.apps.thunderbird.enable {
+    enable = true;
+    policies = {
+      ExtensionSettings = {
+        "{47f5c9df-1d03-5424-ae9e-0613b69a9d2f}" = {
+          installation_mode = "force_installed";
+          install_url = "file://${catppuccinThunderbirdMochaMauve}";
+        };
+      };
+    };
+  };
 }
