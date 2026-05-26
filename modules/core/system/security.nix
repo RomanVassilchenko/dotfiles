a4ebcffb64a98858ef351885b6f91b6f78ff9633
Roman Vassilchenko <roman.vassilchenko.work@gmail.com>
2026-05-26 11:17:01 +0500

    feat: add sudo askpass notification

--- modules/core/system/security.nix
+++ modules/core/system/security.nix
@@ -2,9 +2,39 @@
   config,
   username,
   lib,
+  pkgs,
   ...
 }:
+let
+  sudoAskpass = pkgs.writeShellApplication {
+    name = "dotfiles-sudo-askpass";
+    runtimeInputs = [
+      pkgs.libnotify
+    ];
+    text = ''
+      if [ -z "''${KITTY_WINDOW_ID:-}" ] && [ -n "''${DISPLAY:-}''${WAYLAND_DISPLAY:-}" ]; then
+        notify-send \
+          --app-name=sudo \
+          --urgency=normal \
+          "Sudo password required" \
+          "A graphical password prompt opened because this command is not running in kitty." \
+          >/dev/null 2>&1 || true
+      fi
+
+      exec ${pkgs.kdePackages.ksshaskpass}/bin/ksshaskpass "$@"
+    '';
+  };
+in
 {
+  environment = lib.mkIf config.dotfiles.features.desktop.enable {
+    etc."sudo.conf".text = ''
+      Path askpass ${sudoAskpass}/bin/dotfiles-sudo-askpass
+    '';
+
+    sessionVariables.SUDO_ASKPASS = "${sudoAskpass}/bin/dotfiles-sudo-askpass";
+    systemPackages = [ sudoAskpass ];
+  };
+
   security = {
     rtkit.enable = true;
     forcePageTableIsolation = true;
